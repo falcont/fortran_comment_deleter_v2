0eb7a5abe35a61c03caf77d3fa0f8ccd789a4d21
falcont <tsokolp@gmail.com>
2017-08-18 11:31:56 +0300

    fixies

--- deleter.rb
+++ deleter.rb
@@ -1,11 +1,11 @@
-PATTERN = /^[!|c|C|*|]/
+PATTERN = /^[!|c].*[\r\n]/i
 
 Dir.glob("#{Dir.pwd}/**/*.*") do |file|
   read_file = File.new(file, "r").read
   write_file = File.new(file, "w")
 
   read_file.each_line do |line|
-    write_file.write(line) if line.strip !~ PATTERN
+	write_file.write(line) if line !~ PATTERN
   end
   
   puts "#{file} comments deleted!"
