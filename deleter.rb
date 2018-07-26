204457e77473d67830e250a75014c5a88b89f58e
falcont <tsokolp@gmail.com>
2018-07-26 17:18:47 +0300

    Update deleter.rb

--- deleter.rb
+++ deleter.rb
@@ -1,12 +1,14 @@
-PATTERN = /^[!|c].*[\r\n]/i
-
+# encoding: Windows-1251 
+PATTERN1 = /^[!|c].*[\r\n]/i
+PATTERN2 = /^\s*[!]|^[c].*[\r\n]/i #для пробелов перед !
+PATTERN3 = /^\s*[!]|^[c]|^\s*$.*[\r\n]/i #пустые строки
+#
 Dir.glob("#{Dir.pwd}/**/*.*") do |file|
   read_file = File.new(file, "r").read
   write_file = File.new(file, "w")
-
   read_file.each_line do |line|
-	write_file.write(line) if line !~ PATTERN
+    write_file.write(line) if line !~ PATTERN3
   end
-  
   puts "#{file} comments deleted!"
 end
+#
